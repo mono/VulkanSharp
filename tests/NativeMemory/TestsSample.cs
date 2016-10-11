e2ce68e0a8f9ff91f578f3fe4cb94d19ca1d86d7
Radek Doulik <rodo@xamarin.com>
2016-10-11 18:08:13 +0200

    updated and added new native memory test
     - updated to reworked native memory management, we now test by
       calling GC and checking allocated size
    
     - added test for standalone commands, which were broken - as reported
       in #47

--- tests/NativeMemory/TestsSample.cs
+++ tests/NativeMemory/TestsSample.cs
@@ -33,24 +33,41 @@ namespace NativeMemory
 			if (devices.Length < 1)
 				Assert.Fail ("this test needs device with at least one Vulkan physical device");
 			
-			Console.WriteLine ("enumerated {0} devices, total references {1} allocated size {2}", devices.Length, NativeMemoryDebug.GlobalRefCount, NativeMemoryDebug.AllocatedSize);
+			Console.WriteLine ("enumerated {0} devices, allocated size {1}", devices.Length, NativeMemoryDebug.AllocatedSize);
 			foreach (var device in devices)
 				Console.WriteLine ("device: {0}", device.GetProperties ().DeviceName);
 
-			Console.WriteLine ("before forced GC, total references {0} allocated size {1}", NativeMemoryDebug.GlobalRefCount, NativeMemoryDebug.AllocatedSize);
+			Console.WriteLine ("before forced GC, allocated size {0}", NativeMemoryDebug.AllocatedSize);
 
 			GC.Collect ();
 			GC.WaitForPendingFinalizers ();
-			Console.WriteLine ("after forced GC, total references {0} allocated size {1}", NativeMemoryDebug.GlobalRefCount, NativeMemoryDebug.AllocatedSize);
+			Console.WriteLine ("after forced GC, allocated size {0}", NativeMemoryDebug.AllocatedSize);
 
-			Assert.False (NativeMemoryDebug.GlobalRefCount > 0);
+			Assert.False (NativeMemoryDebug.AllocatedSize > 0);
 
 			instance.Dispose ();
 			instance = null;
 
 			GC.Collect ();
 			GC.WaitForPendingFinalizers ();
-			Console.WriteLine ("after Instance disposed, total references {0} allocated size {1}", NativeMemoryDebug.GlobalRefCount, NativeMemoryDebug.AllocatedSize);
+			Console.WriteLine ("after Instance disposed, allocated size {0}", NativeMemoryDebug.AllocatedSize);
+		}
+
+		[Test]
+		public void CommandsNativeMemory ()
+		{
+			Console.WriteLine ("query layers and extensions info");
+			var layerProps = Commands.EnumerateInstanceLayerProperties ();
+			Console.WriteLine ("after layers info created, allocated size {0}", NativeMemoryDebug.AllocatedSize);
+			var extProps = Commands.EnumerateInstanceExtensionProperties ();
+			Console.WriteLine ("after extensions info created, allocated size {0}", NativeMemoryDebug.AllocatedSize);
+			layerProps = null;
+			extProps = null;
+			Console.WriteLine ("after info dropped, allocated size {0}", NativeMemoryDebug.AllocatedSize);
+			GC.Collect ();
+			GC.WaitForPendingFinalizers ();
+			Console.WriteLine ("after GC and finalizers, allocated size {0}", NativeMemoryDebug.AllocatedSize);
+			Assert.False (NativeMemoryDebug.AllocatedSize > 0);
 		}
 	}
 }
