0b988584974e665ce67fb8d5d266725498ade52d
Alex Hildebrand <alexander.hildebrand@gmail.com>
2018-03-09 10:01:34 +0100

    Vulkan.iOS plattform added

--- src/Vulkan/Properties/AssemblyInfo.cs
+++ src/Vulkan/Properties/AssemblyInfo.cs
@@ -27,3 +27,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Vulkan.Android")]
 [assembly: InternalsVisibleTo("Vulkan.Windows")]
+[assembly: InternalsVisibleTo("Vulkan.iOS")]
